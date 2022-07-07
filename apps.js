a248eecbb6cb8c10565752e2a694b212eca12851
sergio patricio <soporte.tzone@gmail.com>
2022-07-07 09:13:04 -0600

    Instalacion de paqueteria para mysql y prueba de conexion a base de datos

--- apps.js
+++ apps.js
@@ -3,6 +3,38 @@ var express = require('express')
 var path = require('path')
 var logger = require('morgan')
 
+// Se manda llamar la paqueteria para conexion a BD
+var mysql = require('mysql')
+
+//SE establecen los parametros de conexion
+var conexion = mysql.createConnection({
+    host:'192.168.13.32',
+    database:'playground-2',
+    user:'LTMexico',
+    password:'LTMexico'
+});
+
+//Se realiza la validacion de la conexion
+conexion.connect(function(error){
+    if(error){
+        throw error;
+    } else {
+        console.log('CONEXION EXITOSA')
+    }
+});
+
+//Se realiza un query de prueba hacia la bd
+conexion.query('SELECT * from login', function(error,results,fields){
+    if(error)
+        throw error;
+
+        results.forEach(result=>{
+                console.log(result);
+        })
+});
+
+conexion.end();
+
 var adminRouter = require('./routes/admin');
 
 var app = express();
